1a535f474b0d7ef8c92f532eb2ce1b2e371d8323
david wang <david wang>
2017-05-22 14:40:11 +0800

    创建OOP_teamproject工程

---
+++ README.md
@@ -0,0 +1,12 @@
+## OOP team project 
+
+author: 王元炜 高健 琚锡廷
+
+使用tokenize的学术代码剽窃检测程序
+
+----------------------------------------
+
++ classDef.h 表示接口类的构造和接口，该类的文件在根目录下实现
++ 不包含在classDef.h的各个辅助类声明和实现放倒 utils文件夹下
++ [Paper地址]{http://jucs.org/jucs_8_11/finding_plagiarisms_among_a/Prechelt_L.pdf}
++ 除去总类之后，采用最基本的基类进行编程。。可并行化
---
+++ classDef.h
@@ -0,0 +1,69 @@
+// A enumerate type of language 
+enum Language{
+	CPP,
+	Java,
+	Python,
+	LangugeCount
+};
+
+class FileProcessor{
+public:
+	// 注册一个FileProcessor必须提供一个语言
+	FileProcessor();
+	// 释放所有产生的内存
+	~FileProcessor();
+	// 处理一个文件目录下的所有语言认可的文件并合成一个c风格的字符串
+	// 该类将持有所有process产生的字符串，函数析构的时候释放所有。
+	// 可能会被调用多次。。
+	virtual const char *process(const char *pathname) = 0;
+	Language GetLanguage() const;
+};
+
+class CPPfileProcessor: public FileProcessor{
+
+};
+
+class Token{
+	// 检测Token是否相同
+	virtual int ApproximateEqual(const Token *other) const;
+	virtual int Equal(const Token *other) const;
+	// 返回一个可以用于hash的值
+	// 如果Equal Value就相同
+	// 如果ApproximateEqual, ApproximateValue就相同。
+	virtual int Value() const;
+	virtual int ApproximateValue() const; 
+	Language GetLanguage() const;
+};
+
+class Tokenizer{
+	Tokenizer();
+	// 处理经过FileProccessor处理过的源代码
+	void process(const char *source);
+	// 获得Tokens
+	virtual vector<Token*> GetTokens() const;
+	Language GetLanguage() const;
+};
+
+class CPPtokenizer : public Tokenizer{
+
+};
+
+// 这是一个语言无关类，只考虑处理封装好的Tokens.
+class Analyser{
+	Analyser();
+	// 检验两个Token列表， 返回抄袭可能性
+	// 实现见Papar..
+	double check(const vector<Token *>&, const vector<Token *>&);
+};
+
+// 一个 Dectectory
+class PlagiarismDetector{
+	PlagiarismDector(Language lan);
+	double check(const char *path1, const char *path2);
+	map<pair<int, int>, double> check(const char *paths[]);
+};
+
+// 使用以上所有类。。最后实现。。。。
+class CPPPlagiarismDetector : public PlagiarismDector{
+
+};
