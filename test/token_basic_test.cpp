1b8ca577706a3109186645fbb13eea25436d7d57
david wang <david wang>
2017-05-23 19:42:23 +0800

    finish work of basic token and tokenizer

---
+++ test/token_basic_test.cpp
@@ -0,0 +1,15 @@
+// author : davidawng
+#include "../token_basic.h"
+using namespace std;
+
+char buff[10000];
+Tokenizer *tokenizer = new Tokenizer();
+int main(){
+	scanf("%s", buff);
+	tokenizer -> process(buff);
+	auto ret = tokenizer -> GetTokens();
+	for (int i = 0;i < ret.size();i++){
+		printf("%d %d\n", ret[i]->Value(), ret[i] -> ApproximateValue());
+	}
+	return 0;
+}
---
+++ token_basic.h
@@ -0,0 +1,41 @@
+#ifndef OOP_TEAMPROJECT_TOKENBASIC_H
+#define OOP_TEAMPROJECT_TOKENBASIC_H
+// author : davidwang
+#include <cstdio>
+#include <vector>
+
+class Token{
+public:
+	virtual ~Token() = default;
+	Token() = default;
+	Token(char c): c(c){}
+	virtual int ApproximateEqual(const Token *other) const { return this -> c == other -> c; }
+	virtual int Equal(const Token *other) const { return this -> c == other -> c;}
+	virtual int Value() const { return this -> c; }
+	virtual int ApproximateValue() const { return this -> c;} 
+private:
+	char c;
+};
+
+class Tokenizer{
+public:
+	virtual std::vector<const Token *> process(const char *source){
+		tokens.clear();
+		for (int i = 0; source[i]; i++){
+			tokens.push_back(new Token(source[i]));
+		}
+		return tokens;
+	}
+	virtual std::vector<const Token *> GetTokens() const{
+		return tokens;
+	}
+	virtual ~Tokenizer(){
+		for (auto x : tokens){
+			delete x;
+		}
+	}
+protected:
+	std::vector<const Token *> tokens;
+};
+
+#endif // OOP_TEAMPROJECT_TOKENBASIC_H
