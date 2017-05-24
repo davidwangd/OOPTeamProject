a998db2b373a991437ca84bd76421e37e98e9abe
david wang <david wang>
2017-05-24 12:15:43 +0800

    test change

--- test/cpplexer_test.hpp
+++ test/cpplexer_test.hpp
@@ -1,13 +1,1 @@
-class AClass {
-public:
-	AClass();
-private:
-	long double x;
-};
-
-const char* xx = "This is a string test!\n";
-const long double yy = 1.02f;
-
-int main(){
-	return 0;
-}
+long double
