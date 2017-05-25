bda1d24c782de36c639a8cc28c587e6918854702
Daivd Wang <davidwangnoi@163.com>
2017-05-25 21:29:46 +0800

    token finished .. debugging waiting

--- CPPToken.h
+++ CPPToken.h
@@ -16,8 +16,8 @@ class CPPToken : public Token{
 public:
 	// type and id is provided when TokenType is Identifier.. before the first declaration of the identifier..
 	CPPToken(std::pair<CPPLanguage::TokenType, std::string> type, std::string idtype = std::string(""), int id = -1);
-	virtual int Equal() const ;
-	virtual int ApproximateEqual() const;
+	virtual int Equal(const Token *other) const ;
+	virtual int ApproximateEqual(const Token *other) const;
 	virtual int Value() const;
 	virtual int ApproximateValue() const;
 	// This is also returned to help hash usage..
@@ -28,6 +28,7 @@ private:
 	std::string str;
 	std::string idtype;
 	static int MaxId;
+	static std::map<std::string, int> str2int;
 	// to reserve the Token type
 	int id;
 };
